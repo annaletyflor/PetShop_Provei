719c772d83fda43095d8fc65d88723f4c7fa2325
annaletyflor <annaletyflor@gmail.com>
2024-10-31 14:06:47 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShop_Provei.Models
+{
+    public class Cliente
+    {
+        [Key]
+            public Guid ID { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string Nome { get; set; }
+
+            [Required]
+            [RegularExpression(@"^\d{11}$", ErrorMessage = "O CPF deve ter 11 dígitos.")]
+            public string CPF { get; set; }
+
+            [Required]
+            [StringLength(200)]
+            public string Endereco { get; set; }
+
+            [Required]
+            [Phone]
+            public string Telefone { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public bool Ativo { get; set; }
+        }
+    }
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetShop_Provei.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
